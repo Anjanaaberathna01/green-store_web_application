122d852947bcb3ad40f5740655b2f28e460b5ee2
Dulmin wickramage <92096416+Dulmin2021@users.noreply.github.com>
2024-06-01 09:04:44 +0530

    Update IUserConstants.java

--- src/com/shashi/constants/IUserConstants.java
+++ src/com/shashi/constants/IUserConstants.java
@@ -2,6 +2,7 @@ package com.shashi.constants;
 
 public interface IUserConstants {
 
+	
 	public String TABLE_USER = "user";
 	public String COLUMN_NAME = "name";
 	public String COLUMN_MOBILE = "mobile";
